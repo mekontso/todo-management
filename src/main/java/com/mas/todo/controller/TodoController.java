12b5cd7ff07ebfa48b45cfbcbfac17e466b4146d
mekontso <mekontsoanderson@gmail.com>
2024-05-08 06:00:49 +0100

    Basic auth security

--- src/main/java/com/mas/todo/controller/TodoController.java
+++ src/main/java/com/mas/todo/controller/TodoController.java
@@ -5,8 +5,8 @@ import com.mas.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.List;
 
@@ -18,14 +18,13 @@ public class TodoController {
 
     TodoService todoService;
 
-    // addTodo method will be implemented here
     @PostMapping("/add")
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto) {
         TodoDto savedTodo = todoService.addTodo(todoDto);
         return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
     }
 
-    @GetExchange("/get/{id}")
+    @GetMapping("/get/{id}")
     public ResponseEntity<TodoDto> getTodoById(@PathVariable(name = "id") Integer id) {
         TodoDto todo = todoService.getTodoById(id);
         return new ResponseEntity<>(todo, HttpStatus.OK);
@@ -38,22 +37,22 @@ public class TodoController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<TodoDto> updateTodo (@PathVariable(name = "id") Integer id, @RequestBody TodoDto todoDto) {
+    public ResponseEntity<TodoDto> updateTodo(@PathVariable(name = "id") Integer id, @RequestBody TodoDto todoDto) {
         return new ResponseEntity<>(todoService.updateTodoById(id, todoDto), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteTodoById(@PathVariable(name = "id") Integer id){
-        return new ResponseEntity<>(todoService.deleteTodoById(id),HttpStatus.OK);
+    public ResponseEntity<String> deleteTodoById(@PathVariable(name = "id") Integer id) {
+        return new ResponseEntity<>(todoService.deleteTodoById(id), HttpStatus.OK);
     }
 
     @PatchMapping("/complete/{id}")
-    public ResponseEntity<TodoDto> completeTodo(@PathVariable(name = "id") Integer id){
+    public ResponseEntity<TodoDto> completeTodo(@PathVariable(name = "id") Integer id) {
         return new ResponseEntity<>(todoService.completeTodo(id), HttpStatus.OK);
     }
 
     @PatchMapping("/incomplete/{id}")
-    public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable(name = "id") Integer id){
+    public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable(name = "id") Integer id) {
         return new ResponseEntity<>(todoService.inCompelteTodo(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/mas/todo/entity/Todo.java
+++ src/main/java/com/mas/todo/entity/Todo.java
@@ -1,6 +1,5 @@
 package com.mas.todo.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
